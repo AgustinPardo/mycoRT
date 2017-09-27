bfa9dbc15726943adfedd8360960530c9265f10f
agustinpardo <agu_pardo@hotmail.com>
2017-09-27 01:42:12 -0300

    Modificaciones visuales del grafo

--- web/index.js
+++ web/index.js
@@ -1,8 +1,52 @@
 var cy = cytoscape({
-  container: document.getElementById('cy')
+  container: document.getElementById('cy'),
+
+//Stylo del grafo
+
+style: [
+	{
+		selector: 'node', style: {
+			'content':'data(id)',
+        	'background-color': '#df5f5f',
+        	'border-color':'black',
+        	'border-width': 5
+          }
+        },
+	{
+        selector: 'edge', style: {
+            'width': 1,
+            'line-color': '#14a926',
+            'opacity': 0.5
+
+        }
+    },
+
+    {
+        selector: ':parent',
+        style: {
+         	'background-opacity': 0.6,
+         	'background-color': '#e38080'
+        }
+    }
+      ] 
 });
 
-//Request//
+//Agrego los clusters de Operones, es decir motivos
+var eles = cy.add([
+	{ data: { id: '3873039', parent: 'b' }},
+    { data: { id: 'b' } },
+    { data: { id: '1132034', parent: 'b' } },
+    { data: { id: '1132850', parent: 'b' } },
+    { data: { id: '1132110', parent:'e' }},
+    { data: { id: '3873198', parent:'e' }},
+    { data: { id: 'e' } },
+    { data: { id: '1132143', parent: 'e' } }
+]);
+
+//La data de los mitivos la puedo poner en el json..
+
+
+//Request nodos y aristas//
 fetch('/red', {
 	method: 'get'
 }).then(function(response) {
@@ -10,26 +54,31 @@ fetch('/red', {
 }).then(function(data) {
 	
 	data.nodos.forEach( x =>{
-		console.log(x.rv)
 		var eles = cy.add(
+		//Cargo nodos con el id de Operon
 		{group:"nodes", data:{ id: x.rv}})});
+
 var i = 0;
 	data.aristas.forEach(y =>{		
 		
 		i++;
 	var eles = cy.add(
+		//Cargo aristas con un nuevo id..
 		{group: "edges", data:{ id: i.toString() , source:y.op1 , target: y.op2 }}
 	)
 	});
 	
-	var layout = cy.layout( { name: 'grid'} );
+//Diseño del grafo
+
+//No me anda el layout "spread"..{ name: 'spread',minDist:40}
+	var layout = cy.layout( { name: 'random'});
 	layout.run();
 	
 }).catch(function(err) {
 	console.log(err)
 });
 
-
+//document.getElementById("pruebita").innerHTML = "";
 
 
 
