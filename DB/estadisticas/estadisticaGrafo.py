cbb5cbbe76674eab267322c239538eb634231bd0
Agustin <agu_pardo@hotmail.com>
2017-12-18 17:06:24 -0300

    Utilizacion de liberia powerLaw

--- DB/estadisticas/estadisticaGrafo.py
+++ DB/estadisticas/estadisticaGrafo.py
@@ -1,31 +1,74 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import pandas as pd
+import powerlaw
+
 
 # DiGraphs hold directed edges. Self loops are allowed but multiple (parallel) edges are not.
-g = nx.read_edgelist("/home/agustin/redRegulatoria_H37RV/mycoRT/DB/2_redT_conOperones_net_number.csv",delimiter=",", nodetype=int, create_using=nx.DiGraph())
-print(g.edges())
+# g = nx.read_edgelist("/home/agustin/redRegulatoria_H37RV/mycoRT/DB/2_redT_conOperones_net_number.csv",delimiter=",", nodetype=int, create_using=nx.DiGraph())
 
-numero_nodos=nx.number_of_nodes(g)
-print(numero_nodos)
+entrada = pd.read_csv("/home/agustin/redRegulatoria_H37RV/mycoRT/DB/2_redT_conOperones_net_number.csv",header=None)
+entrada_tuplas = [tuple(x) for x in entrada.values]
+
+ug = nx.Graph()
+ug.add_edges_from(entrada_tuplas)  # undirected global graph; edgetable is just a list of A->B, A->C, C->D etc.
+
+# largest connected component
+Gc = max(nx.connected_component_subgraphs(ug), key=len)
+
+g = nx.DiGraph()
+g.add_edges_from(Gc.edges())  # directed global graph from the largest connected component
+
+
+
+numero_nodos_ug=nx.number_of_nodes(ug)
+print(numero_nodos_ug)
+
+numero_nodos_g=nx.number_of_nodes(g)
+print(numero_nodos_g)
+
+
 numero_ejes=nx.number_of_edges(g)
-print(numero_ejes)
-
 betweenness_centrality=nx.betweenness_centrality(g)
-print(betweenness_centrality)
 
-distribucion_grados=nx.degree_histogram(g)
-print(distribucion_grados)
-
-# nx.draw(g)
-# plt.show()
-
-# import powerlaw
-# fit = powerlaw.Fit(data)
-# fit.power_law.alpha
-# fit.power_law.sigma
+def grados(distribucion):
+    degree_list = []
+    for x in distribucion:
+        if x[1]!=0:
+            degree_list.append(x[1])
+    return degree_list
 
 
+data2=grados(g.in_degree)
+data=grados(g.out_degree)
+
+plt.plot(sorted(data,reverse=True))
+plt.show()
 
 
+fit = powerlaw.Fit(data)
+print(fit.power_law.alpha)
+print(fit.power_law.sigma)
 
+####
+print(powerlaw.pdf(data))
+powerlaw.plot_pdf(data,linewidth=2)
+
+powerlaw.plot_pdf(data, linear_bins=True, color='r')
+plt.show()
+
+powerlaw.plot_cdf([data])
+
+#
+# figPDF = powerlaw.plot_pdf(data, color='b')
+# powerlaw.plot_pdf(data, linear_bins=True, color='r', ax=figPDF)
+# fit.power_law.plot_pdf(color='b', linestyle='--', ax=figPDF)
+# ####
+# figPDF.set_ylabel("p(X)")
+# figPDF.set_xlabel(r"In degree")
+# figname = 'FigPDF'
+# plt.savefig(figname+'.eps', bbox_inches='tight')
+# #savefig(figname+'.tiff', bbox_inches='tight', dpi=300)
+
+plt.show()
 
